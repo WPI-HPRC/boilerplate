35985c8744cbb6ca908ac84faff5a7a3e08c3438
coburndaniel979 <coburndaniel979@gmail.com>
2025-02-07 17:48:03 -0500

    move sensor manager to .h file, change how timer works

--- Sensors/SensorManager/SensorManager.h
+++ Sensors/SensorManager/SensorManager.h
@@ -6,19 +6,60 @@
 
 #include <vector>
 #include "../Sensor/Sensor.h"
+#include <Arduino.h>
 
-class SensorManager {
+template<typename MillisFn> class SensorManager {
 public:
-    bool addSensor(Sensor* sensorPtr); // add sensor, true if added, false if not added
-    bool removeSensor(Sensor* sensorPtr); // remove sensor, true if success
-    void run(); // going to be called
-    virtual ~SensorManager() = default; // un-allocation
-    bool sensorInit(); // true if success false if something fail
+
+    /**
+    @brief Run before use 
+    */
+    void initialize() {
+        this->startTime = millis();
+        initialize_impl();
+    }
+
+    // adds a new sensor to the SensorManager
+    bool addSensor(Sensor* sensorPtr) {
+        auto it = std::find(sensors.begin(), sensors.end(), sensorPtr);
+
+        if (it == sensors.end()) {
+            sensors.push_back(sensorPtr);
+            return true;
+        }
+
+        return false;        
+    }
+
+    // read the sensors
+    void** loop() {
+        void** data = new void*[sensors.size()];
+
+        for (size_t i = 0; i < sensors.size(); i++) {
+            if (sensors[i]->getInitStatus()) {
+                long currentTime = this->millis();
+                if (sensors[i]->getLastTimeRead() + sensors[i]->getPollingPeriod() >= currentTime) {
+                    data[i] = sensors[i]->update(currentTime);
+
+                }
+            }
+        }        
+    }
+        
+    bool sensorInit() {
+        bool success = true;
+        for(size_t i = 0; i< sensors.size(); i++) {
+            sensors[i]->init();
+            success = success && sensors[i]->getInitStatus();
+        }
+        return success;
+    } // true if success false if something fail
 
 private:
     void** readSensors(); // array of reading pointers
-    Time* timer;
     std::vector<Sensor*> sensors;
+    virtual void initialize_impl() = 0;
+    MillisFn millis; // time
 
 protected:
 };
