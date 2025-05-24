d13814d773dc3a95997064f6bae001f682c22672
Dashiell Elliott <dashiell.elliott@yahoo.com>
2025-05-24 19:42:42 -0400

    change how looper parameters work

--- Looper/Looper.h
+++ Looper/Looper.h
@@ -2,12 +2,20 @@
 
 #include "HardwareTimer.h"
 #include <Arduino.h>
+#include <cassert>
 #include <cstdint>
 #include <initializer_list>
 
 using loop_fn_t = void (*)();
 
-template <loop_fn_t... fns> class Looper {
+template <loop_fn_t...> struct FunctionsList {};
+
+template <uint32_t...> struct FunctionDelaysList {};
+
+template <typename FList, typename DelayList> class Looper;
+
+template <loop_fn_t... fns, uint32_t... fn_delays>
+class Looper<FunctionsList<fns...>, FunctionDelaysList<fn_delays...>> {
   public:
     /* @params
      - Polling delay in microseconds. Make this smaller than the smallest
@@ -18,16 +26,14 @@ template <loop_fn_t... fns> class Looper {
      least 10.
 
      - TIM instance to use for the hardware timer, such as TIM1, TIM2, etc..
-     Make sure it is not in use elsewhere.
+     Make sure it is not in use elsewhere (i.e., don't use TIM6/TIM7, as they
+     are used by the Tone and Servo libraries).
 
      - List of delays in microseconds corresponding to functions in the template
      parameter.
     */
-    template <typename... Args>
-    Looper(uint32_t pollingDelay, uint32_t prio, TIM_TypeDef *timInst,
-           Args... fn_delays)
-        : pollingDelay(pollingDelay), prio(prio), timer(timInst),
-          delays{fn_delays...} {}
+    Looper(uint32_t pollingDelay, uint32_t prio, TIM_TypeDef *timInst)
+        : pollingDelay(pollingDelay), prio(prio), timer(timInst) {}
 
     void init() {
         timer.setOverflow(pollingDelay, MICROSEC_FORMAT);
@@ -37,22 +43,24 @@ template <loop_fn_t... fns> class Looper {
         timer.resume();
     }
 
+    static_assert(sizeof...(fns) == sizeof...(fn_delays));
+
   private:
     uint32_t pollingDelay;
     uint32_t prio;
     HardwareTimer timer;
-    uint32_t delays[sizeof...(fns)];
     uint32_t lastCall[sizeof...(fns)] = {};
 
     void handler() {
         uint64_t now = micros();
         size_t i = 0;
-        for (auto f : std::initializer_list<loop_fn_t>{fns...}) {
-            if (now - lastCall[i] >= delays[i]) {
+        auto f_list = std::array<loop_fn_t, sizeof...(fns)>{fns...};
+        auto d_list = std::array<uint32_t, sizeof...(fn_delays)>{fn_delays...};
+        for (size_t i = 0; i < sizeof...(fns); i++) {
+            if (now - lastCall[i] >= d_list[i]) {
                 lastCall[i] = now;
-                f();
+                f_list[i]();
             }
-            i++;
         }
     }
 };
