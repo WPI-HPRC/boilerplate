95779d52c95c70390913304679c286901d704b0b
Dashiell Elliott <dashiell.elliott@yahoo.com>
2025-05-25 10:10:42 -0400

    make the function delays be in millis instead of micros

--- Looper/Looper.h
+++ Looper/Looper.h
@@ -14,10 +14,13 @@ template <uint32_t...> struct FunctionDelaysList {};
 
 template <typename FList, typename DelayList> class Looper;
 
+//! @template_params
+//! - List of function pointers
+//! - Corresponding delays in ms
 template <loop_fn_t... fns, uint32_t... fn_delays>
 class Looper<FunctionsList<fns...>, FunctionDelaysList<fn_delays...>> {
   public:
-    /* @params
+    /** @params
      - Polling delay in microseconds. Make this smaller than the smallest
      delay for a function handled by this looper.
 
@@ -28,9 +31,6 @@ class Looper<FunctionsList<fns...>, FunctionDelaysList<fn_delays...>> {
      - TIM instance to use for the hardware timer, such as TIM1, TIM2, etc..
      Make sure it is not in use elsewhere (i.e., don't use TIM6/TIM7, as they
      are used by the Tone and Servo libraries).
-
-     - List of delays in microseconds corresponding to functions in the template
-     parameter.
     */
     Looper(uint32_t pollingDelay, uint32_t prio, TIM_TypeDef *timInst)
         : pollingDelay(pollingDelay), prio(prio), timer(timInst) {}
@@ -43,7 +43,8 @@ class Looper<FunctionsList<fns...>, FunctionDelaysList<fn_delays...>> {
         timer.resume();
     }
 
-    static_assert(sizeof...(fns) == sizeof...(fn_delays));
+    static_assert(sizeof...(fns) == sizeof...(fn_delays),
+                  "There must be the same number of functions and delays");
 
   private:
     uint32_t pollingDelay;
@@ -52,10 +53,11 @@ class Looper<FunctionsList<fns...>, FunctionDelaysList<fn_delays...>> {
     uint32_t lastCall[sizeof...(fns)] = {};
 
     void handler() {
+        constexpr auto f_list = std::array<loop_fn_t, sizeof...(fns)>{fns...};
+        constexpr auto d_list =
+            std::array<uint32_t, sizeof...(fn_delays)>{1000 * fn_delays...};
         uint64_t now = micros();
         size_t i = 0;
-        auto f_list = std::array<loop_fn_t, sizeof...(fns)>{fns...};
-        auto d_list = std::array<uint32_t, sizeof...(fn_delays)>{fn_delays...};
         for (size_t i = 0; i < sizeof...(fns); i++) {
             if (now - lastCall[i] >= d_list[i]) {
                 lastCall[i] = now;
