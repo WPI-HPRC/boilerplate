62f356ea725f0056a7949a27fac3d63607c60da0
Abhay <abhaymathur1000@gmail.com>
2025-11-05 20:43:18 -0500

    Chris is the best ever!

--- StateEstimator/AttMEKF.cpp
+++ StateEstimator/AttMEKF.cpp
@@ -208,30 +208,110 @@ BLA::Matrix<13,1> AttStateEstimator::propGyro(BLA::Matrix<3,1> u)
     BLA:Matrix<4,1> qNorm = q / BLA::Norm(q);
 }
 
-BLA::Matrix<13, 1> AttStateEstimator::predictionFunction(BLA::Matrix<3, 1> u) {
+BLA::Matrix<12, 12> AttStateEstimator::predictionFunction(float dt) {
     float gyrX = magData->gyrX;
     float gyrY = magData->gyrY;
     float gyrZ = magData->gyrZ;
 
+    /*
     float aclX = magData->accelX;
     float aclY = magData->accelY;
     float aclZ = magData->accelZ;
 
-    float magX = magData->magX;
-    float magY = magData->magY;
-    float magZ = magData->magZ;
-
-    BLA::Matrix<3, 1, float> gyroMag = {gyrX, gyrY, gyrZ};
+    BLA::Matrix<4, 1> q =
+    {
+    x(AttMEKFInds::q_w),
+    x(AttMEKFInds::q_x),
+    x(AttMEKFInds::q_y),
+    x(AttMEKFInds::q_z)
+    }
+    BLA::Matrix<3, 3> rotMatrix = QuaternionUtils::quatToRot(q);
     
-    BLA::Matrix<3,3, float> gyroSkew = QuaternionUtils::skewSymmetric(gyroMag);
+    BLA::Matrix<3, 1> accelVec = {accelX, accelY, accelZ};
+    BLA::Matrix<3,3> accelSkew = QuaternionUtils::skewSymmetric(accelVec);
+    */
+
+    BLA::Matrix<3, 1> gyroVec = {gyrX, gyrY, gyrZ};
+    BLA::Matrix<3,3> gyroSkew = QuaternionUtils::skewSymmetric(gyroVec);
 
     BLA::Matrix<12, 12> F;
     F = F.Fill(0);
 
-    skewQ = QuaternionUtils::skewSymmetric()
-
-    F(0, 0) = -1 * QuaternionUtils::skewSymmetric()
-
+    //Row 1 - 3
+    F.subMatrix<3, 3>(0, AttMEKFInds::q_w) = -1 * gyroSkew; 
+    F.subMatrix<3, 3>(0, AttMEKFInds::gb_x) = -1 * I_3;
     F()
+
+    BLA::Matrix<12, 12> phi;
+    P = P.Fill(0);
+
+    phi = I_12 + (F * dt) + (0.5 * F * F * pow(dt, 2));
 }
 
+BLA::Matrix<12, 12> AttStateEstimator::measurementFunction(BLA::Matrix<3, 1> u, float dt, bool inPreLaunch) {
+    float magX = magData->magX;
+    float magY = magData->magY;
+    float magZ = magData->magZ;
+
+    float aclX = magData->accelX;
+    float aclY = magData->accelY;
+    float aclZ = magData->accelZ;   
+
+    BLA::Matrix<3, 1> accelVec = {accelX, accelY, accelZ};
+    BLA::Matrix<3, 1> magVec = {magX, magY, magZ};
+
+
+    BLA::Matrix<3,1> g = {0, 0, -9.81};
+
+    BLA::Matrix<4,1> q = {
+        x_in(AttMEKFInds::q_w),
+        x_in(AttMEKFInds::q_x),
+        x_in(AttMEKFInds::q_y),
+        x_in(AttMEKFInds::q_z)
+    };
+
+    BLA::Matrix<3,3> rotMatrix = quat2rot(q);
+
+    BLA::Matrix<3, 12> H_accel;
+    H_accel = H_accel.fill(0);
+    H_accel.subMatrix<3, 3>(0, AttMEKFInds::q_w) = rotMatrix * g;
+    H_accel.subMatrix<3, 3>(0, AttMEKFInds::ab_x - 1) = I_3;
+
+    BLA::Matrix<3, 12> H_mag;
+    H_mag = H_mag.fill(0);
+       //TODO
+    H_mag.subMatrix<3, 3>(0, AttMEKFInds::q_w) = rotMatrix * 67; //come back and add igrm_model       H_mag.subMatrix<3 ,3>(0, AttMEKFInds::mb_x - 1) = I_3;
+
+    BLA::Matrix<3, 3> accel_h;
+    accel_h = accel_h.fill(0);
+    accel_h = QuaternionUtils::quat2DCM(q) * g
+
+    BLA::Matrix<3, 3> mag_h;
+    mag_h = mag_h.fill(0);
+    //Come back igrm whatever
+    mag_h = QuaternionUtils::quat2DCM(q) * 67 
+
+    if(inPreLaunch){
+        BLA::Matrix<6, 12> H;
+        H = H.fill(0);
+        H.subMatrix<3, 12>(0, 0) = H_accel;
+        H.subMatrix<3, 12>(3, 0) = H_mag;
+        
+        BLA::Matrix<6, 1> h;
+        h = h.fill(0);
+        H.subMatrix<3, 1>(0, 0) = accel_h;
+        H.subMatrix<3, 1>(3, 0) = mag_h;
+
+        BLA::Matrix<6, 1> residual;
+        residual.subMatrix<3, 1>(0, 0) = accelVec;
+        residual.subMatrix<3, 1>(3, 0) = magVec;
+        residual = residual - h;
+    }
+    else{
+        BLA::Matrix<3, 12> H = H_mag;
+        BLA::Matrix<3, 1> h = mag_h;
+        BLA::Matrix<3, 1> residual = magVec - h;
+    }
+
+    
+}
